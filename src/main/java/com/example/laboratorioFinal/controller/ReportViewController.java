4025280d942744ced25c8073fb879296abf2d257
Cristhian C <ceballoscristhian4@gmail.com>
2022-10-17 09:18:16 -0500

    Ultimos cambios 1.0

--- src/main/java/com/example/laboratorioFinal/controller/ReportViewController.java
+++ src/main/java/com/example/laboratorioFinal/controller/ReportViewController.java
@@ -1,115 +1,55 @@
 package com.example.laboratorioFinal.controller;
 
 import com.example.laboratorioFinal.HelloApplication;
-import com.example.laboratorioFinal.model.Student;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.Objects;
-import java.util.ResourceBundle;
 
-public class ReportViewController implements Initializable {
-    private String search;
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
+public class ReportViewController {
 
     @FXML
-    private TableColumn<?, ?> cantiEl;
+    private TableColumn<?, ?> colCantiLoan;
 
     @FXML
-    private TableColumn<?, ?> careerEs;
+    private TableColumn<?, ?> colDeudorLoan;
 
     @FXML
-    private TextField codigoReporte;
+    private TableColumn<?, ?> colDeudorLoan1;
 
     @FXML
-    private TableColumn<?, ?> idEl;
+    private TableColumn<?, ?> colDeudorLoan11;
 
     @FXML
-    private TableColumn<?, ?> idEs;
+    private TableColumn<?, ?> colElementLoan;
 
     @FXML
-    private TableColumn<?, ?> localEl;
+    private TableColumn<?, ?> colEntregaLoan;
 
     @FXML
-    private TableColumn<?, ?> nameEl;
+    private TableColumn<?, ?> colPrestamoLoan;
 
     @FXML
-    private TableColumn<?, ?> nameEs;
+    private TextField inputBuscar;
 
     @FXML
-    private TextField nombreBuscarE;
+    private TableView<?> tblLoan;
+
 
     @FXML
-    private TableColumn<?, ?> priceEl;
-
-    @FXML
-    private TableColumn<?, ?> semEs;
-
-    @FXML
-    private TableColumn<?, ?> stateEl;
-
-    @FXML
-    private TableView<?> tableReportEst;
-
-    @FXML
-    private TableColumn<?, ?> typeEl;
-
-    @FXML
-    private TableColumn<?, ?> useEl;
-
-    @FXML
-    void buscarCodigoR(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buscarNombre(ActionEvent event) {
-
-    }
-
-    @FXML
-    void buscarPrestamoE(ActionEvent event) {
-        getData();
-    }
-
-    @FXML
-    void cantidadPrestamo(ActionEvent event) {
-
+    void botonBuscar(ActionEvent event) {
     }
 
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
 
-    }
     @FXML
     void MainView(ActionEvent event) throws IOException {
         HelloApplication.mainViews(event);
     }
-    @FXML
-    void getData(){
-        search = String.valueOf(this.nombreBuscarE.getText());
-    }
-    @FXML
-    void fillTable(Student student, Student student1){
-        student.setName(student1.getName());
-        student.setId(student1.getId());
-        student.setCareer(student1.getCareer());
-        student.setSemester(student1.getSemester());
-    }
+
+
 }
 
